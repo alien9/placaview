cdbe3c8c087ede27a6c24b4dde0a2ad0212ebfc8
Tiago Barufi <barufi@gmail.com>
2024-08-02 12:05:41 -0400

    ãuto-installing

--- tools.py
+++ tools.py
@@ -1,19 +1,27 @@
 from urllib.parse import urlparse
+import os
+import sys, time
+import subprocess
+from pathlib import Path
 
 try:
     from mapbox_vector_tile import decode
 except (ModuleNotFoundError, ImportError):
     print('Module mapbox-vector-tile not found. Installing from PyPi')
-    import pip
-    pip.main(['install', 'mapbox-vector-tile'])
+    install_requirements()
+    time.sleep(8)
+    #import pip
+    #pip.main(['install', 'mapbox-vector-tile'])
     from mapbox_vector_tile import decode
    
 try:
     from vt2geojson.features import Layer
 except (ModuleNotFoundError, ImportError):
     print('Module vt2geojson not found. Installing from PyPi')
-    import pip
-    pip.main(['install', 'vt2geojson'])
+    install_requirements()
+    time.sleep(10)
+    #import pip
+    #pip.main(['install', 'vt2geojson'])
     from vt2geojson.features import Layer
 
 def _is_url(uri: str) -> bool:
@@ -42,3 +50,33 @@ def vt_bytes_to_geojson(b_content: bytes, x: int, y: int, z: int, layer=None) ->
         "type": "FeatureCollection",
         "features": [f for fc in features_collections for f in fc["features"]]
     }
+
+def install_requirements():
+    #definition of the bat file as as string
+    #it will be equivalent to manually type these commands line by line
+    batF="""@echo off
+        call "->QGISPATH<-\\o4w_env.bat"
+        call "py3_env"
+        call python -m pip install mapbox-vector-tile
+        call python -m pip install vt2geojson
+        call exit
+        @echo on
+        """
+
+    # then the idea is to find the osgeo4w shell path
+    # sys.executable is the base execution path and contains o4w_env.bat
+    # o4w_env.bat setup the osgeo4w shell
+    qgispath = str(os.path.dirname(sys.executable))
+
+    # here you replace the string ->QGISPATH<- with qgis path
+    # so that the script is installation independant
+    batF = batF.replace("->QGISPATH<-",qgispath)
+
+    # and the string ->HOMEPATH<- with the path to your requirements.txt
+
+    # then you write it to a .bat file and run it
+
+    with open("INSTALL.bat","w") as f:
+        f.write(batF)
+
+    subprocess.run(["INSTALL.bat"])
