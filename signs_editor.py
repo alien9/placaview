4d2be8325a135b189d802dee29f385f584cd4bb6
Tiago Barufi <barufi@gmail.com>
2024-08-26 12:45:12 -0300

    adding style

--- signs_editor.py
+++ signs_editor.py
@@ -145,7 +145,7 @@ class SignsEditor(QMainWindow, FormClass):
 
     def write_autocomplete(self, field_name, values):
         with open(f"{os.path.dirname(__file__)}/styles/autocomplete/{field_name}.txt", "a+") as fu:
-            for v in values:
+            for v in list(set(values)):
                 fu.write(f"{v}\n")
             fu.close()
         completer = QCompleter(values, self)
