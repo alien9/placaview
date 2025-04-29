1c83db674b3c6fe2fa9d8e3f82c053fec1f8d0f9
Tiago Barufi <barufi@gmail.com>
2025-04-29 10:23:11 -0300

    adding the browser

---
+++ signs_insert.py
@@ -0,0 +1,36 @@
+from qgis.core import QgsVectorLayer, QgsFeature, QgsField, QgsGeometry, QgsPointXY, QgsField, QgsProject
+from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication, Qt, QVariant, pyqtSlot, QObject, pyqtSignal
+from qgis.PyQt.QtGui import QIcon
+from qgis.PyQt.QtWidgets import QAction, QInputDialog, QLineEdit, QLabel, QMessageBox, QProgressDialog, QProgressBar
+from qgis.core import QgsProject, QgsWkbTypes, QgsMapLayer, QgsVectorFileWriter
+from qgis.core import QgsCoordinateTransform, QgsCoordinateTransformContext, QgsCoordinateReferenceSystem, QgsGeometry, QgsPoint
+from qgis.core import QgsCategorizedSymbolRenderer
+from qgis.PyQt import uic
+from qgis.core import QgsStyle, QgsSymbol,QgsRendererCategory, QgsSvgMarkerSymbolLayer
+from qgis.gui import QgsMapToolEmitPoint, QgsMapToolIdentify
+
+class SignsInsert(QgsMapToolEmitPoint):
+
+    signInserted = pyqtSignal(object, object)
+
+    def __init__(self, iface, layer):
+        self.iface = iface
+        self.canvas = self.iface.mapCanvas()
+        self.layer = layer #self.iface.activeLayer()
+        QgsMapToolEmitPoint.__init__(self, self.canvas)
+        self.iface.mapCanvas().setMapTool(self)
+
+    def insert_sign_at(self, *args, **kwargs):
+        print("inserting sifng")
+        print(args)
+                    
+    def canvasReleaseEvent(self, mouseEvent):
+        print("canvas release")
+            
+    def canvasPressEvent(self, event):
+        print("presss")
+        
+    def deactivate(self):
+        self.layer.removeSelection()
+        
+        
