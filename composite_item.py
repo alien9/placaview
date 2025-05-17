43c447e8360137f4dbfd1f9747980f38884b1d9b
Tiago Barufi <barufi@gmail.com>
2025-05-17 00:14:18 -0300

    adicionado novas telas

---
+++ composite_item.py
@@ -0,0 +1,45 @@
+from qgis.core import QgsVectorLayer, QgsFeature, QgsField, QgsGeometry, QgsPointXY, QgsField, QgsProject
+from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication, Qt, QVariant
+from qgis.PyQt.QtGui import QIcon
+from qgis.PyQt.QtWidgets import QAction, QInputDialog, QLineEdit, QLabel, QMessageBox, QProgressDialog, QProgressBar, QDialog, QWidget,QPushButton, QListView, QListWidget, QListWidgetItem, QCheckBox
+from qgis.core import QgsProject, QgsWkbTypes, QgsMapLayer, QgsVectorFileWriter
+from qgis.core import QgsCoordinateTransform, QgsCoordinateTransformContext, QgsCoordinateReferenceSystem, QgsGeometry, QgsPoint
+from qgis.core import QgsCategorizedSymbolRenderer
+from qgis.PyQt.QtWidgets import QApplication, QWidget,  QLineEdit,  QFormLayout,  QHBoxLayout
+from qgis.PyQt import uic, QtSvg
+import os, re
+from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication, Qt, QVariant, pyqtSlot, QObject, pyqtSignal
+from qgis.gui import QgsFilterLineEdit
+
+class CompositeItem(QWidget):
+    check:QCheckBox
+    changed = pyqtSignal(str, bool, int, int)
+    
+    def __init__(self, sign_name, sign_text, sign_id, composite_id, parent=None):
+        super(CompositeItem, self).__init__(parent)
+        self.row = QHBoxLayout()
+        if not sign_name:
+            name="unknown_sign.svg"
+        else:
+            name=sign_name
+        svgWidget = QtSvg.QSvgWidget(os.path.join(os.path.dirname(__file__), "styles", name))
+        svgWidget.setFixedWidth(30)
+        svgWidget.setFixedHeight(30)
+        self.row.addWidget(svgWidget)
+        self.check=QCheckBox(text=sign_text)
+        self.check.name=name
+        self.row.addWidget(self.check)
+        self.setLayout(self.row)
+        self.name=name
+        self.sign_id=sign_id
+        self.composite_id=composite_id
+        self.check.stateChanged.connect(self.valueChanged)
+        
+    def setValue(self, value):
+        self.check.setChecked(value)
+        
+    def valueChanged(self, *args):
+        self.changed.emit(self.name, args[0]==2, self.sign_id, self.composite_id)
+
+    def getValue(self):
+        return self.check.isChecked()
---
+++ composite_selector.py
@@ -0,0 +1,78 @@
+from qgis.core import QgsVectorLayer, QgsFeature, QgsField, QgsGeometry, QgsPointXY, QgsField, QgsProject
+from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication, Qt, QVariant, pyqtSlot, QObject, pyqtSignal
+from qgis.PyQt.QtGui import QIcon
+from qgis.PyQt.QtWidgets import QAction, QInputDialog, QLineEdit, QLabel, QMessageBox, QProgressDialog, QProgressBar, QDialog, QWidget,QPushButton, QListView, QListWidget, QListWidgetItem, QCheckBox
+from qgis.PyQt import QtSvg
+from qgis.core import QgsProject, QgsWkbTypes, QgsMapLayer, QgsVectorFileWriter
+from qgis.core import QgsCoordinateTransform, QgsCoordinateTransformContext, QgsCoordinateReferenceSystem, QgsGeometry, QgsPoint
+from qgis.core import QgsCategorizedSymbolRenderer
+from qgis.PyQt.QtWidgets import QApplication, QWidget,  QLineEdit,  QFormLayout,  QHBoxLayout
+from qgis.PyQt.QtWidgets import QApplication, QWidget,  QLineEdit, QCompleter, QTextEdit,  QFormLayout,  QHBoxLayout, QGraphicsView, QVBoxLayout, QApplication, QWidget, QVBoxLayout, QGraphicsView, QGraphicsScene
+from qgis.PyQt import uic
+#from qgis.gui import (QgsFieldComboBox, QgsMapLayerComboBox)
+from qgis.core import QgsMapLayerProxyModel
+from qgis.gui import QgsFilterLineEdit
+from .composite_item import CompositeItem
+from qgis.core import NULL
+
+import os, re
+
+FormClass, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'composite_selector.ui'))
+
+class CompositeSelector(QDialog, FormClass):
+    sign_names=None
+    sign=None
+    selected_signs=set()
+    applyClicked = pyqtSignal(list, str)
+    placas=[]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(parent=kwargs.get("parent"))
+        self.setWindowTitle("Composite Sign")
+        self.sign=kwargs.get("sign")
+        self.layer=kwargs.get("layer")
+        self.placas=kwargs.get("placas")
+        self.roads=kwargs.get("roads")
+        self.conf=kwargs.get("conf")
+        self.setupUi(self)
+        self.findChild(QPushButton, "pushButton_ok").clicked.connect(self.close)
+        n=0
+
+        vbox=QVBoxLayout()  
+        for placa in self.placas:
+            
+            
+            widget=self.findChild(QListWidget, "listWidget")
+            item = QListWidgetItem(widget)
+            icon=placa["value_code_face"]
+            if icon is None or icon==NULL or icon=="":
+                icon=placa["value"]
+            if placa["road"]==NULL:
+                item.name="Unknown road"
+            else:
+                item.name=self.roads.getFeature(placa["road"])[self.conf.get("roads_field_name")]
+            widget.addItem(item)
+            row=CompositeItem(icon, str(item.name), placa.id(), self.sign["composite_id"])
+            row.setValue(self.sign["composite_id"]==placa["composite_id"])
+            row.changed.connect(self.valueChanged)
+            item.setSizeHint(row.minimumSizeHint())
+            widget.setItemWidget(item, row)
+            n+=1
+            
+    def valueChanged(self, *args, **kwargs):
+        
+        
+        self.layer.startEditing()
+        f=self.layer.getFeature(args[2])
+        
+        
+        if args[1]:
+            f.setAttribute(self.layer.fields().indexOf("composite_id"),args[3])
+        else:
+            f.setAttribute(self.layer.fields().indexOf("composite_id"),NULL)
+        self.layer.updateFeature(f)
+        self.layer.commitChanges()
+        
+        
+
